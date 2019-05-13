81a976dbb31797506fb3aa940cb842b661e0227b
Idontwantyourcloud <flolippert@t-online.de>
2019-05-13 20:27:34 +0200

    added vector v calculation for calc_P2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-struct quat { long quat[4]; };
-
 void setup() {
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
@@ -9,19 +7,36 @@ void setup() {
   pinMode(6, OUTPUT);
 }
 
-void calc_PID(struct quat q_m) {
-  long q_ref[4], q_konjm[4], q_err[3][3], tau[3];
+void calc_PID(long *q_ref, long *q_m, long *tau_arr, long *PID_arr) {
+  long q_konjm[4], q_err[3][3], v[9];
+
+  q_konjm[0]=q_m[0];
+  q_konjm[1]=-q_m[1];
+  q_konjm[2]=-q_m[2];
+  q_konjm[3]=-q_m[3];
+
   for (int a=1; a<4; a++) {
     q_err[0][a] = q_ref[1] * q_konjm[a];
     q_err[1][a] = q_ref[2] * q_konjm[a];
     q_err[2][a] = q_ref[3] * q_konjm[a];
   }
+
+  //calculate vector v:
+  v[0] = q_m[0]*q_m[0] + q_m[1]*q_m[1] - q_m[2]*q_m[2] - q_m[3]*q_m[3];
+  v[1] = 2 * (q_m[1]*q_m[2] + q_m[0]*q_m[3]);
+  v[2] = 2 * (q_m[1]*q_m[3] - q_m[0]*q_m[2]);
+  v[3] = 2 * (q_m[1]*q_m[2] - q_m[0]*q_m[3]);
+  v[4] = q_m[0]*q_m[0] - q_m[1]*q_m[1] + q_m[2]*q_m[2] - q_m[3]*q_m[3];
+  v[5] = 2 * (q_m[2]*q_m[3] + q_m[0]*q_m[1]);
+  v[6] = 2 * (q_m[1]*q_m[3] + q_m[0]*q_m[2]);
+  v[7] = 2 * (q_m[2]*q_m[3] - q_m[0]*q_m[1]);
+  v[8] = q_m[0]*q_m[0] - q_m[1]*q_m[1] - q_m[2]*q_m[2] + q_m[3]*q_m[3];
 }
 
 void loop() {
-  struct quat new_quat;
+  long q_ref_arr[4], q_m_arr[4], tau_arr[3], PID_arr[3];
 
-  calc_PID(new_quat);
+  calc_PID(q_ref_arr, q_m_arr, tau_arr, PID_arr);
 }
 
 void Loop_Motors() {
